16ae7dc96444c120a45a48646b71406a832edcf6
rgrannell1 <r.grannell2@gmail.com>
2014-07-08 22:02:57 +0100

    updated parser

--- example/examples.js
+++ example/examples.js
@@ -1,7 +1,17 @@
 
-[
+lisbParse = require('../lib/lisb-parse').lisbParse
+
+const quote  = 'quote'
+const cond   = 'cond'
+const let    = 'let'
+const λ      = 'λ'
+const begin  = 'begin'
+
+
+
+lisbParse([
 	begin,
 	[let, ':x', 10],
 	[let, ':y', [
 		'*', ':x', ':x']]
-]
+])

eef6d8cafa2b271edd22093d90d8b707bc5e9a7a
rgrannell1 <r.grannell2@gmail.com>
2014-07-13 19:06:34 +0100

    implemented quasi-quotation

--- example/examples.js
+++ example/examples.js
@@ -1,27 +1,27 @@
 
 lEval = require('../lib/lisb-eval').lEval
 
-const let     = 'let'
-const cond    = 'cond'
-const fn      = 'fn'
-const begin   = 'begin'
-const list    = 'list'
-const quote   = 'quote'
-const unquote = 'unquote'
-
+const let        = 'let'
+const cond       = 'cond'
+const fn         = 'fn'
+const begin      = 'begin'
+const list       = 'list'
+const quote      = 'quote'
+const unquote    = 'unquote'
+const quasiquote = 'quasiquote'
 
 console.log( lEval([
 	begin,
 
 	[let,
 		':code', [
-		quote,
+		quasiquote,
 			[
 				begin,
 				[let, ':x', 0],
-				[let, ':y', 1]
-				[unquote, ]
-
+				[let, ':y', 1],
+				[unquote,
+					['+', 1, 2]]
 			]] ],
 
 	[':code']

239f685ca024b9b6f5af7064f635d9f1d4d35226
rgrannell1 <r.grannell2@gmail.com>
2014-07-19 01:49:42 +0100

    top level eval now has workign options

--- example/examples.js
+++ example/examples.js
@@ -4,8 +4,20 @@ lEval = require('../lib/lisb-eval').lEval
 const let        = 'let'
 const cond       = 'cond'
 const fn         = 'fn'
+const mr         = 'mr'
 const begin      = 'begin'
 const list       = 'list'
 const quote      = 'quote'
 const unquote    = 'unquote'
 const quasiquote = 'quasiquote'
+
+
+
+
+
+
+lEval([':clog', 100], {
+
+	debug: true
+
+})
